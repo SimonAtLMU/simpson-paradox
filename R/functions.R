c23f8a2ace9cf377fbc1aa98c35afa67ba0ab2e2
Simon <simon.herrmann@campus.lmu.de>
2023-07-23 23:41:01 +0200

    improved the functions

--- R/functions.R
+++ R/functions.R
@@ -11,7 +11,7 @@
 #' @returns A gentle introduction to the simpson package
 #' @export
 hello_simpson <- function() {
-print("In this package you will learn more about a statistical phenomenon that was named after a british statistician named Edward Hugh Simpson. As we will see, the outcomes of this phenomenon are sometimes quite curious and paradox. To learn more about these statistical curiosities, use the function get_started(). As input for this function, you need to chose a dataset. For educational purposes, we provide two example datasets in this package. Do you want to work with data on smoking behaviour and running speed? The chose the dataset called dash. Are you more interested in the development of wages in the US and how it differs in different educational subgroups? Then chose the dataset wages.")
+message("In this package you will learn more about a statistical phenomenon that was named after a british statistician named Edward Hugh Simpson. As we will see, the outcomes of this phenomenon are sometimes quite curious and paradox. To learn more about these statistical curiosities, use the function explore(). As input for this function, you need to chose a dataset. For educational purposes, we provide two example datasets in this package. Do you want to work with data on smoking behaviour and running speed? The chose the dataset called dash. Are you more interested in the development of wages in the US and how it differs in different educational subgroups? Then chose the dataset wages.")
 }
 
 #' @title Explore the datasets
@@ -54,7 +54,7 @@ explore <- function(df) {
         y = "Wage per month",
       )
 
-    message("Take a look at the scatterplot on the right! Hmm... that looks kinda odd. When considering the regression line, it looks like that the monthly wages of US citizens rose overall since 2005. But by taking a look at the cases in the scatterplot, it doesn't look like that the wages didn't really increase. Let's double check that by calculating a regression analysis. Please use the function regression_all() for this purpose. As before, use wages as input of the function.")
+    message("Take a look at the scatterplot on the right! Hmm... that looks kinda odd. When considering the regression line, it looks like that the monthly wages of US citizens rose overall since 2005. But by taking a look at the cases in the scatterplot, it doesn't look like that the wages really increased. Let's double check that by calculating a regression analysis. Please use the function regression_all() for this purpose. As before, use wages as input of the function.")
 
     return(plot)
 
@@ -178,7 +178,7 @@ compare <- function(df) {
 
     plot_merged <- plot_grid(plot1, plot2, labels = c('Total', 'Grouped'), label_size = 9)
 
-    message("The thing we can see right here is a perfect example of the Simpson Paradox. We get different results depending on the groups we take into account in our regression analysis: when looking at all students, smoking cigarettes seems to have a positive impact on running speed (or negative on the time needed ;)). But when we differ between sexes, we see that smoking has a negative effect on running speed. The Simpson paradox occurs because the two sexes differ in one characteristic that we did not consider at all in the first analysis: smoking behaviour. We see that male students tend to smoke more that female students. Just because boys run faster than girls on average, the regression analyses of the whole group suggests that smoking more make you run faster. But as we know, that's untrue! So always think of confunding variables when developing a study or analyzing data :)")
+    message("The thing we can see right here is a perfect example of the Simpson Paradox. We get different results depending on the groups we take into account in our regression analysis: when looking at all students, smoking cigarettes seems to have a positive impact on running speed (or negative on the time needed - shown in the scatterplot). But when we differ between sexes, we see that smoking has a negative effect on running speed (Students that smoke more need more time for the 100-meter dash). The Simpson paradox occurs because the two sexes differ in one characteristic that we did not consider at all in the first analysis: smoking behaviour. We see that male students tend to smoke more than female students. Just because boys run faster than girls on average (maybe because of physiological differences), the regression analyses of the whole group suggests that smoking more make you run faster. But as we know, that's untrue! So always think of confunding variables when developing a study or analyzing data. The Simpson Paradox might be in your data :)")
 
     return(plot_merged)
 
@@ -214,7 +214,7 @@ compare <- function(df) {
 
     plot_merged <- plot_grid(plot1, plot2, labels = c('Total', 'Grouped'), label_size = 9)
 
-    message("The thing we can see right here is a perfect example of the Simpson Paradox. We get different results depending on the groups we take into account in our regression analysis: when looking at all citizens (Total), it seems like wages increasd slightly over the years. But when we look at the educational subgroups (Grouped), we see that in all of these groups wages have shrunk. How is this possible? Well, the explanation for this lies in the changing educational profile of workforce in the US since 2005. As you can see from the data, more and the number of people making a college degree (red dots) increased. On that, the wages for college graduates have fallen at a much slower rate than for people in other educational subgroups. The increase of people making a college degree and the relatively slower rate of wage decrease in this group leads to different trends. So always keep in mind that your perception of data can change depending on your viewpoint on it. Don't trust easily the results of a single analysis. :)")
+    message("The thing we can see right here is a perfect example of the Simpson Paradox. We get different results depending on the groups we take into account in our regression analysis: when looking at all citizens (Total), it seems like wages increasd slightly over the years. But when we look at the educational subgroups (Grouped), we see that in all of these groups wages decreased. How is this possible? Well, the explanation for this lies in the changing educational profile of workforce in the US since 2005. As you can see from the data, the number of people making a college degree (red dots) increased. On that, the wages for college graduates have fallen at a much slower rate than for people in other educational subgroups. The increase of people making a college degree and the relatively slower rate of wage decrease in this group is responsible for a misleading result of the first regression analysis. So always keep in mind that your perception of data can change depending on your viewpoint on it. Don't easily trust the results of a single analysis. :)")
 
     return(plot_merged)
 
